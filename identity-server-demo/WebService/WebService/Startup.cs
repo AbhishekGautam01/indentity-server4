b354368edc2e9ecad537883fcbedff5009fa7d81
AbhishekGautam01 <er.abhishek.gautam@outlook.com>
2021-01-21 19:28:29 +0530

    [Configured]: Startup to add IdentityServer

--- identity-server-demo/WebService/WebService/Startup.cs
+++ identity-server-demo/WebService/WebService/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SpaServices.AngularCli;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using WebService.Models;
 
 namespace WebService
 {
@@ -17,9 +18,9 @@ namespace WebService
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddIdentityCore<AppUser>(options => { });
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             // In production, the Angular files will be served from this directory
@@ -29,7 +30,6 @@ namespace WebService
             });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
