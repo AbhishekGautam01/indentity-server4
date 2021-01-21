7babc1318d3b3bc685093dad1ef43579cf49ac68
AbhishekGautam01 <er.abhishek.gautam@outlook.com>
2021-01-21 19:28:07 +0530

    [Created]: App User Model

---
+++ identity-server-demo/WebService/WebService/Models/AppUser.cs
@@ -0,0 +1,11 @@
+﻿namespace WebService.Models
+{
+    public class AppUser
+    {
+            public string Id { get; set; }
+            public string UserName { get; set; }
+            public string Email { get; set; }
+            public string NormalizeUserName { get; set; }
+            public string PasswordHash { get; set; }
+    }
+}
