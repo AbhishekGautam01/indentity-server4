b0eb9427e2c0e2919fa57c20ee67fdac6413eadd
AbhishekGautam01 <er.abhishek.gautam@outlook.com>
2021-01-21 19:29:19 +0530

    [Created]: Account Controller & Impl User Create Method

---
+++ identity-server-demo/WebService/WebService/Controllers/AccountController.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using WebService.Models;
+using WebService.ViewModels;
+
+namespace WebService.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly UserManager<AppUser> _userManager;
+        public AccountController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        [HttpPost]
+        public async Task<ResultVM> Register([FromBody]RegisterVM model)
+        {
+            if (ModelState.IsValid)
+            {
+                IdentityResult result = null;
+                var user = await _userManager.FindByNameAsync(model.UserName);
+
+                if (user != null)
+                {
+                    return new ResultVM
+                    {
+                        Status = Status.Error,
+                        Message = "Invalid data",
+                        Data = "<li>User already exists</li>"
+                    };
+                }
+
+                user = new AppUser
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    UserName = model.UserName,
+                    Email = model.Email
+                };
+
+                result = await _userManager.CreateAsync(user, model.Password);
+
+                if (result.Succeeded)
+                {
+                    return new ResultVM
+                    {
+                        Status = Status.Success,
+                        Message = "User Created",
+                        Data = user
+                    };
+                }
+                else
+                {
+                    var resultErrors = result.Errors.Select(e => "<li>" + e.Description + "</li>");
+                    return new ResultVM
+                    {
+                        Status = Status.Error,
+                        Message = "Invalid data",
+                        Data = string.Join("", resultErrors)
+                    };
+                }
+            }
+
+            var errors = ModelState.Keys.Select(e => "<li>" + e + "</li>");
+            return new ResultVM
+            {
+                Status = Status.Error,
+                Message = "Invalid data",
+                Data = string.Join("", errors)
+            };
+        }
+    }
+}
