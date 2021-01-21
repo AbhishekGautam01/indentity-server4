bc05526569bef83ac4db74dea2091343a9e51770
AbhishekGautam01 <er.abhishek.gautam@outlook.com>
2021-01-21 19:28:55 +0530

    [Created]: User Repo

---
+++ identity-server-demo/WebService/WebService/Infrastructure/UserRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using WebService.Models;
+
+namespace WebService.Infrastructure
+{
+    public static class UserRepository
+    {
+        public static List<AppUser> Users;
+
+        static UserRepository()
+        {
+            Users = new List<AppUser>();
+        }
+    }
+}
