8a0aec32a05bf470aa3c4ea29371bbe2db5ad944
AbhishekGautam01 <er.abhishek.gautam@outlook.com>
2021-01-22 18:58:45 +0530

    Login Endpoint

--- identity-server-demo/WebService/WebService/Controllers/AccountController.cs
+++ identity-server-demo/WebService/WebService/Controllers/AccountController.cs
@@ -1,7 +1,10 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using WebService.Models;
 using WebService.ViewModels;
@@ -73,5 +76,39 @@ namespace WebService.Controllers
                 Data = string.Join("", errors)
             };
         }
-    }
+        [HttpPost]
+        public async Task<ResultVM> Login([FromBody] LoginVM model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.FindByNameAsync(model.UserName);
+                if(user != null && await _userManager.CheckPasswordAsync(user, model.Password))
+                {
+                    var identity = new ClaimsIdentity(CookieAuthenticationDefaults.AuthenticationScheme);
+                    identity.AddClaim(new Claim(ClaimTypes.NameIdentifier, user.Id));
+                    identity.AddClaim(new Claim(ClaimTypes.Name, user.UserName));
+                    await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(identity));
+                    return new ResultVM
+                    {
+                        Status = Status.Success,
+                        Message = "Succesfull login",
+                        Data = model
+                    };
+                }
+                return new ResultVM
+                {
+                    Status = Status.Error,
+                    Message = "Invalid data",
+                    Data = "<li>Invalid Username or Password</li>"
+                };
+            }
+            var errors = ModelState.Keys.Select(e => "<li>" + e + "</li>");
+            return new ResultVM
+            {
+                Status = Status.Error,
+                Message = "Invalid data",
+                Data = string.Join("", errors)
+            };
+        }
+        }
 }
